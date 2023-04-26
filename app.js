2b9613d9ea9828e702838acd5f57e3a03537be6d
julianvalerio <julianrvalerio89@gmail.com>
2023-04-26 16:53:34 -0300

    Identificando KeyCode

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var main = function(){
 	"use strict";
-	$(".comment-input button").on("click", 
-		function(event){
-			var $novo_comentario = $("<p>"),comment_text = $(".comment-input input").val();
+	$(".comment-input button").on("click", function(event){
+		if($(".comment-input input").val() !== ""){
+			var $novo_comentario = $("<p>"),
+			comment_text = $(".comment-input input").val();
 			$novo_comentario.text(comment_text);
+			$novo_comentario.hide();//Esconde o texto
 			$(".comments").append($novo_comentario);
+			$novo_comentario.fadeIn();// Exibe gradualmente
+			$(".comment-input input").val("");
+		}
 	});
 };
+
+$(".comment-input input").on("keypress", function(event){
+	console.log("keyCode "+ event.keyCode);
+});
 
 $(document).ready(main);
