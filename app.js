8d271a2f1c97378f4fa6cf9d5ad96202563b5fef
julianvalerio <julianrvalerio89@gmail.com>
2023-04-27 15:46:26 -0300

    Codigo otimizado

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 var main = function(){
 	"use strict";
-	$(".comment-input button").on("click", function(event){
+	var addComentario = function(){
+		var $novo_comentario;
 		if($(".comment-input input").val() !== ""){
-			var $novo_comentario = $("<p>"),
-			comment_text = $(".comment-input input").val();
-			$novo_comentario.text(comment_text);
-			$novo_comentario.hide();//Esconde o texto
-			$(".comments").append($novo_comentario);
-			$novo_comentario.fadeIn();// Exibe gradualmente
+			$novo_comentario = $("<p>").text($(".comment-input input").val());
+			$novo_comentario.hide();
+			$(".comments").append($novo_comentario);	
+			$novo_comentario.fadeIn();
 			$(".comment-input input").val("");
+		}
+	};
+	$(".comment-input button").on("click", function(event){
+		addComentario();
+	});
+	$(".comment-input input").on("keypress", function(event){
+		if(event.keyCode === 13){
+			addComentario();
 		}
 	});
 };
-
-$(".comment-input input").on("keypress", function(event){
-	console.log("keyCode "+ event.keyCode);
-});
-
 $(document).ready(main);
